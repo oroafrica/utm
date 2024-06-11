ceb6b19e5d664570534792b3c97696e810f626b1
moegamat <moe.toffar@affiliate.imd.org>
2024-06-11 22:56:06 +0200

    property adjust

--- orosource.js
+++ orosource.js
@@ -1,25 +1,34 @@
 (async () => {
     const { log, error } = console;
 
-    log("loading orosource: https://oroafrica.github.io/utm/orosource.js");
+    log("loading orosource: https://oroafrica.github.io/utm/orosource.js v1.00");
 
-    const processLocation = () => {
+    const processLocation = () => 
+    {
         const searchParams = new URLSearchParams(window.location.search);
         let params = {};
-        searchParams.forEach((value, key) => { params[key] = value; });
-        log(JSON.stringify(params, null, 2));
+        searchParams.forEach((value, key) => { params[key] = decodeURIComponent(value); });
+        window.localStorage.setItem("params", JSON.stringify({params}));
 
-        let urlParams = new URL(document.location).searchParams;
-        let name = urlParams.get("utm_source");
-        window.localStorage.setItem("params", JSON.stringify({name}));
-        log("target: ", name);
-        log("host: ", searchParams);
+        const affiliate = (key)=> 
+        {
+            let obj= [
+            {name:"ntombi",email:"ntombi@ntombijewels.net",phone:"+27 (0)21 930 5897",
+                profile:"https://www.oroafrica.com/images/companies/48/NY%20image_Page_04.jpg?1709727266390"},
+            {name:"kara",email:"kara@sparkles.com",phone:"+27 (0)31 521 3978",
+                profile:"https://www.oroafrica.com/images/companies/48/NY%20image_Page_01.jpg?1709728387620"
+            }];
+ 
+            return obj.filter(n=> n.name == key)[0];
+        };
 
         let _email = document.querySelector(".utm_email");
         let _phone = document.querySelector(".utm_phone");
-        log(_email.innerText, " — ", _phone.innerText);
-        _email.innerText = "misty@blues.com";
-        _phone.innerText = "+41 25698 4589";
+        let _profile = document.querySelector(".utm_profile");
+
+        _email.innerText = affiliate(params.utm_source).email;
+        _phone.innerText = affiliate(params.utm_source).phone;
+        _profile.src = affiliate(params.utm_source).profile;
     };
 
     document.addEventListener('DOMContentLoaded', processLocation);
---
+++ orosource_1.js
@@ -0,0 +1,27 @@
+(async () => {
+    const { log, error } = console;
+
+    log("loading orosource: https://oroafrica.github.io/utm/orosource.js");
+
+    const processLocation = () => 
+    {
+        const searchParams = new URLSearchParams(window.location.search);
+        let params = {};
+        searchParams.forEach((value, key) => { params[key] = value; });
+        log(JSON.stringify(params, null, 2));
+
+        let urlParams = new URL(document.location).searchParams;
+        let name = urlParams.get("utm_source");
+        window.localStorage.setItem("params", JSON.stringify({name}));
+        log("target: ", name);
+        log("host: ", searchParams);
+
+        let _email = document.querySelector(".utm_email");
+        let _phone = document.querySelector(".utm_phone");
+        log(_email.innerText, " — ", _phone.innerText);
+        _email.innerText = "misty@blues.com";
+        _phone.innerText = "+41 25698 4589";
+    };
+
+    document.addEventListener('DOMContentLoaded', processLocation);
+})();
