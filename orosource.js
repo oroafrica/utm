2a3dd192ddc30bf5633a2d2766e12647ed906f67
moegamat <moe.toffar@affiliate.imd.org>
2024-06-11 20:36:08 +0200

    update js

--- orosource.js
+++ orosource.js
@@ -6,7 +6,10 @@
     const searchParams = new URLSearchParams(window.location.search);
     searchParams.forEach((value, key) => { params[key] = value; });
     log(JSON.stringify(params,null,2));
-    
-    localStorage.setItem("params",JSON.stringify(params));
+    let params = new URL(document.location).searchParams;
+    let name = params.get("utm_source")
+    localStorage.setItem("params",name);
+    log("target: ",name);
 }
 )()
+new URLSearchParams(window.location.search)
