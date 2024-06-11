b5977e0635a9d1a806719e72d22ee3b766959a0f
moegamat <moe.toffar@affiliate.imd.org>
2024-06-11 19:48:34 +0200

    add local storage

--- orosource.js
+++ orosource.js
@@ -2,9 +2,11 @@
 {
     const {log,error} = console;
 
-    log("loading orosource");
+    log("loading orosource: https://oroafrica.github.io/utm/orosource.js");
     const searchParams = new URLSearchParams(window.location.search);
     searchParams.forEach((value, key) => { params[key] = value; });
     log(JSON.stringify(params,null,2));
+    
+    localStorage.setItem("params",JSON.stringify(params));
 }
 )()
