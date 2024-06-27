c159188d3c1d6c107b9a996e798b731cf263c177
oroafrica <moegamat@oroafrica.com>
2024-06-27 08:12:01 +0200

    update affiliate

--- affiliateStore.js
+++ affiliateStore.js
@@ -7,36 +7,27 @@
 
     log("loading orosource: https://oroafrica.github.io/utm/affiliateStore.js v2.0.0");
 
-    const processStore = async () => 
-    {
-        const payload = {"script":"affiliateStore.js","version":"1.0.0"};
-
-        let lastPathSegment = window.location.pathname.split('/').filter(Boolean).pop() || '';
-        lastPathSegment = lastPathSegment.replace(/\.html$/, '');
-        payload.affiliate = lastPathSegment;
-        payload.home = window.location.href
-        payload.tel = document.querySelector(".__cf_tel_client__").textContent;
-        payload.email = document.querySelector(".__cf_email_client__").textContent;
-
-        const searchParams = new URLSearchParams(window.location.search);
-        let params = {};
-        searchParams.forEach((value, key) => { params[key] = decodeURIComponent(value); });
-        payload.params= params;
-        window.localStorage.setItem(KEYS, JSON.stringify({payload}));
-
-
+    const processStore = async () => {
+        const payload = {
+            script: "affiliateStore.js",
+            version: "1.0.0",
+            affiliate: window.location.pathname.split('/').pop().replace(/\.html$/, '') || '',
+            home: window.location.href,
+            params: Object.fromEntries(new URLSearchParams(window.location.search))
+        };
+    
+        window.localStorage.setItem(KEYS, JSON.stringify({ payload }));
+    
         const storage = JSON.parse(window.localStorage.getItem(KEYS));
-        if(typeof storage === "object" || storage !== undefined)
-        {
-            document.querySelector(".__cf_tel__")?.textContent = (storage.tel !== undefined) ? storage.tel : "+27 (0) 21 480 9860";
-            document.querySelector(".__cf_email__")?.textContent = (storage.email !== undefined) ? storage.email : "orders@oroafrica.com";
-            log(JSON.stringify(payload, null, 2) , " :source data");
-        }
-        else 
-        {
+        if (storage && typeof storage === "object") {
+            document.querySelector(".__cf_tel__")?.textContent = storage.tel || "+27 (0) 21 480 9860";
+            document.querySelector(".__cf_email__")?.textContent = storage.email || "orders@oroafrica.com";
+            log(JSON.stringify(payload, null, 2), " :source data");
+        } else {
             log("Error on localStorage!");
         }
     };
+    
 
     document.addEventListener('DOMContentLoaded', processStore);
     window.addEventListener('hashchange', processStore);
