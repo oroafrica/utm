864590debb851bf418961c92a7109c66be80e0d0
moegamat <moe.toffar@affiliate.imd.org>
2024-07-04 08:09:34 +0200

    add annotations

--- affiliate.js
+++ affiliate.js
@@ -2,15 +2,13 @@
     
     const debug = true;
     const KEYS = "affiliate";
-
-    const log = (msg)=>(debug) ? console.log("DEV: ", msg) : null;
-
-    log("loading orosource: https://oroafrica.github.io/utm/affiliate.js v2.0.0","\n","capture affiliate name");
+    const VERSION = "2.0.0";
+    const log = (msg)=>(debug) ? console.log("DEBUG MODE: ", msg) : null;
 
     const processLocation = async () => 
     {
-        const payload = {"script":"affiliate.js","version":"1.0.1"};
-
+        const payload = {"script":`${KEYS}`,"version":`${VERSION}`};
+        //scrape affiliate details: adding home here to dynamicly acquire the affiliate homepage     
         let lastPathSegment = window.location.pathname.split('/').filter(Boolean).pop() || '';
         lastPathSegment = lastPathSegment.replace(/\.html$/, '');
         payload.affiliate = lastPathSegment;
@@ -19,14 +17,12 @@
         payload.tel =  document.querySelector(".__cf_tel__")?.textContent || "";
         payload.logo =  document.querySelector(".__cf_logo__")?.textContent || "";
         payload.home =  document.querySelector(".__cf_home__")?.href || "";
-
+        //scrape search params in event we add additional information
         const searchParams = new URLSearchParams(window.location.search);
         let params = {};
         searchParams.forEach((value, key) => { params[key] = decodeURIComponent(value); });
         payload.params= params;
-
-        
-
+        //load to both storage providers to test persistence
         window.localStorage.setItem(KEYS, JSON.stringify({payload}));
         window.sessionStorage.setItem(KEYS, JSON.stringify({payload}));
 
